88638aa6493eb41548e3ff5585f74b18a03e0206
waclawik <kasiaw359@gmail.com>
2025-05-27 22:43:09 +0200

    Initial commit

---
+++ Kod.R
@@ -0,0 +1,41 @@
+# 1. Prezentacja i opis danych
+# Opis wykorzystywanych danych i ich źródło.
+# Analiza podstawowych statystyk (np. średnia, odchylenie, kurtoza, skośność, współczynnik zmienności).
+# Analiza współczynników korelacji pomiędzy zmiennymi.
+# Wykresy zależności pomiędzy zmiennymi.
+
+
+# 2. Odpowiednie przygotowanie danych (jeśli będzie taka konieczność)
+# Badanie stacjonarności zmiennych i eliminacja niestacjonarności (w przypadku szeregów czasowych).
+# Logarytmowanie.
+# Eliminacja wartości odstających.
+
+# 3. Zastosowanie wybranej metody doboru zmiennych do modelu (np. metoda Hellwiga,metoda krokowa wsteczna)
+
+# 4. Budowa modelu
+
+# 5. Weryfikacja poprawności modelu
+# Badanie normalności rozkładu reszt.
+# Testowanie autokorelacji (w przypadku modelu budowanego na szeregach czasowych).
+# Badanie heteroskedastyczności.
+# Testowanie współliniowości (VIF).
+# Testowanie stabilności parametrów modelu (test Chowa).
+# Testowanie stabilności postaci analitycznej modelu (test Ramsey’a RESET, test liczby serii).
+# Badanie efektu katalizy.
+# Badanie koincydencji.
+
+
+# W przypadku, gdy któryś z testów zwróci niepożądany rezultat należy przekształcić
+# model (np. dokonać pewnych transformacji zmiennych, wybrać inny podzbiór
+#        zmiennych objaśniających, wyeliminować którąś ze zmiennych, spróbować usunąć
+#        autokorelację/heteroskedastyczność). Może nie udać się znaleźć modelu, który
+# spełniał będzie wszystkie założenia. Ważne, by podjęta została próba poradzenia
+# sobie z problemem.
+
+
+# 6. Prezentacja ostatecznej postaci modelu i jego ocena.
+# Ocena istotności zmiennych (test t-Studenta i test Walda).
+# Ocena współczynnika determinacji.
+# Interpretacja parametrów modelu.
+
+# 7. Wyznaczenie prognozy punktowej. Obliczenie względnego błędu prognozy EX POST.
